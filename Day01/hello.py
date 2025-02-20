bf903cced9e07a088ad7d59fc6c152ebe996503b
Waliullah <waliullah9277>
2025-02-20 22:37:48 +0600

    some program add

---
+++ Day01/hello.py
@@ -0,0 +1,16 @@
+# print('hello')
+n = int(input('Enter any num: '))
+# if n%2==0:
+#     print(n,'is even number!')
+# else:
+#     print(n,'is odd number!')
+
+if n > 0:
+    print(n,'is positive number!')
+elif n < 0:
+    print(n,'is negative number!')
+else:
+    print('This number is zero!')
+
+print('hello bangladesh')
+print('hello world!')
---
+++ Day01/loop.py
@@ -0,0 +1,3 @@
+n = int(input('Enter any num: '))
+for i in range(1, n+1):
+    print(i,'I Love Python!')
